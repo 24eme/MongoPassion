1da42f80d9d792e388b9b0b4d5a084e3d625d4d8
rilerss <58331611+rilerss@users.noreply.github.com>
2020-09-07 11:14:21 +0200

    Add files via upload

--- model/model.php
+++ model/model.php
@@ -3,27 +3,45 @@
 function getDocument()
 {
 	require 'vendor/autoload.php';
-	$test = "mongodb://localhost:27017";
-	$client = new MongoDB\Client(strval($test));
-	$collection = $client->insert_test->insert;
-	$id = new MongoDB\BSON\ObjectId("5f3fa61fc8d24006841d3802");
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+	$db = strval($_SESSION['db']);
+	$collec = strval($_SESSION['collection']);
+	$collection = $client->$db->$collec;
+
+	// $documents = $client->$db->request("db.getCollection(".$collection.").find(".$requete")";
+
+	if($_GET['type_id']=='object'){
+		$id = new MongoDB\BSON\ObjectId($_GET['id']);
+	}
+	else{
+		$id = $_GET['id'];
+	}
+
+	$_SESSION['doc'] = $_GET['id'];
+
 	$result = $collection->find(['_id'=>$id]);
 	return $result;
 }
 
 function improved_var_export ($variable) {
 	$send = $variable;
-	if($variable instanceof stdClass){
-		$sendable=false;
-	    while ($sendable==false) {
-	    	$send = array();
-	    	foreach ($variable as $key => $value) {
-	    		$send[$key]=improved_var_export($value);
-	    		$sendable=true;
-	    	}
-	    }
-	}
-	$sendable=true;
+    if($variable instanceof stdClass){
+    	$sendable=false;
+    	while ($sendable==false) {
+    		$send = array();
+    		foreach ($variable as $key => $value) {
+    			$send[$key]=improved_var_export($value);
+    			$sendable=true;
+    		}
+    	}
+    }
+    if(gettype($variable)=='array'){
+    	foreach ($variable as $key => $value) {
+    			$send[$key]=improved_var_export($value);
+    			$sendable=true;
+    	}
+    }
+    $sendable=true;
 	return $send;
 }
 
@@ -42,7 +60,6 @@ function printable($obj){
 		}		
 		$docs = json_encode($print);
 		$print = json_decode($docs);
-
 	}
 	$printable=true;
 	return $print;
@@ -50,13 +67,422 @@ function printable($obj){
 
 function getLink_doc()
 {
-	$link_doc='index.php?action=traitement_uD&id=5f3fa61fc8d24006841d3802';
+	$link_doc='index.php?action=traitement_uD&id='.$_SESSION['doc'].'&type_id='.$_GET['type_id'];
 
 	return $link_doc;
 }
 
 
-function updateDoc()
+function searchError_doc_id()
 {
+	$error_id = false;
+
+	$dec = json_decode($_POST['doc_text']);
+	$test = improved_var_export($dec);
+
+	if($_GET['type_id']=='object'){
+		if(gettype($test['_id'])!='array'){
+			$error_id = true;
+		}
+	}
+	else{
+		if($test['_id']!=$_GET['id']){
+			$error_id = true;
+		}
+	}
+	return $error_id;
+}
+
+
+function getUpdate_doc()
+{
+	$dec = json_decode($_POST['doc_text']);
+	$test = improved_var_export($dec);
+
+	unset($test['_id']);
+
+	$date_array = unserialize($_POST['date_array']);
+	$up_date_array = unserialize($_POST['up_date_array']);
+	if(!empty($date_array)){
+		foreach ($date_array as $x=>$x_value) {
+			$temp = strtotime($test[$x]['date'])*1000;
+			$diff = $up_date_array[$x]-$temp;
+			$time = $x_value - $diff;
+			$date = new MongoDB\BSON\UTCDateTime($time);
+			$test[$x] = $date;
+		}
+	}
+	return $test;
+}
+
+function getDoc_id()
+{
+	if($_GET['type_id']=='object'){
+		$id = new MongoDB\BSON\ObjectId($_GET['id']);
+	}
+	else{
+		$id = $_GET['id'];
+	}
+	return $id;
+}
+
+function updateDoc($id,$doc)
+{
+	require 'vendor/autoload.php';
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+	$db = strval($_SESSION['db']);
+	$collec = strval($_SESSION['collection']);
+	$collection = $client->$db->$collec;
+	$collection->updateOne(
+    [ '_id' => $id ],
+    [ '$set' => $doc]);
+}
+
+function init_json($doc){
+	if(gettype($doc)=="array"){
+		$ok=false;
+		while($ok==false){
+			foreach ($doc as $key => $value) {
+				$doc[$key]=init_json($value);
+				$ok=true;
+			}
+		}
+	}
+	elseif (gettype($doc)=="string") {
+		if(strpos($doc, "\\")!==false){
+			$doc = addslashes($doc);
+		}
+		$ok = true;
+	}
+	$ok=true;
+	return $doc;
+}
+
+function getDocs($page,$bypage)
+{
+	require 'vendor/autoload.php';
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+	$db = strval($_SESSION['db']);
+	$collec = strval($_SESSION['collection']);
+	$collection = $client->$db->$collec;
+
+	$skip = ($page-1)*$bypage;
+
+	$result = $collection->find([],['skip'=>$skip,'limit'=>$bypage])->toArray();
+
+	return $result;
+}
+
+function countDocs()
+{
+	require 'vendor/autoload.php';
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+	$db = strval($_SESSION['db']);
+	$collec = strval($_SESSION['collection']);
+	$collection = $client->$db->$collec;
+	$result = $collection->count([]);
+	return $result;
+
+}
+
+function getNew_doc()
+{
+	$dec = json_decode($_POST['doc_text']);
+	$test = improved_var_export($dec);
+	return $test;
+}
+
+function insertDoc($doc)
+{
+	require 'vendor/autoload.php';
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+	$db = strval($_SESSION['db']);
+	$collec = strval($_SESSION['collection']);
+	$collection = $client->$db->$collec;
+
+	$collection->insertOne($doc);
+}
+
+function deleteDoc()
+{
+	require 'vendor/autoload.php';
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+	$db = strval($_SESSION['db']);
+	$collec = strval($_SESSION['collection']);
+	$collection = $client->$db->$collec;
+
+	if($_GET['type_id']=='object'){
+		$id = new MongoDB\BSON\ObjectId($_GET['id']);
+	}
+	else{
+		$id = $_GET['id'];
+	}
+
+	$collection->deleteOne(['_id'=>$id]);
+}
+
+function getSearch_id($search,$page,$bypage){
+	require 'vendor/autoload.php';
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+	$db = strval($_SESSION['db']);
+	$collec = strval($_SESSION['collection']);
+	$collection = $client->$db->$collec;
+
+	$skip = ($page-1)*$bypage;
 	
+	$regex = new MongoDB\BSON\Regex ($search);
+	$result1 = $collection->find(['_id'=>$regex],['skip'=>$skip,'limit'=>$bypage])->toArray();
+
+	try{
+		$test = new MongoDB\BSON\ObjectId($search);
+		$result2 = $collection->find(['_id'=>$test],['skip'=>$skip,'limit'=>$bypage])->toArray();
+	}
+	catch(Exception $e){
+		$result2 = array();
+	}
+
+	$result = array_merge($result1,$result2);
+
+	return $result;
+}
+
+function getSearch_g($search,$page,$bypage){
+	require 'vendor/autoload.php';
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+	$db = strval($_SESSION['db']);
+	$collec = strval($_SESSION['collection']);
+	$collection = $client->$db->$collec;
+
+	$skip = ($page-1)*$bypage;
+
+	$tab_search = explode('=', $search);
+
+	$tab_find = array();
+
+	foreach ($tab_search as $x) {
+		$y = trim($x);
+		array_push($tab_find, $y);
+	}
+
+	$result = $collection->find([$tab_find[0] => $tab_find[1]],['skip'=>$skip,'limit'=>$bypage])->toArray();
+
+
+	return $result;
+}
+
+function getSearch($search_id,$search_g,$page,$bypage){
+
+	if($search_id == ''){
+
+		$result = getSearch_g($search_g,$page,$bypage);
+	}
+
+	elseif($search_g=='field = content[...]'){
+		$result = getSearch_id($search_id,$page,$bypage);;
+	}
+
+	else{
+
+		require 'vendor/autoload.php';
+		$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+		$db = strval($_SESSION['db']);
+		$collec = strval($_SESSION['collection']);
+		$collection = $client->$db->$collec;
+
+		$skip = ($page-1)*$bypage;
+
+		$tab_search = explode('=', $search_g);
+
+		$tab_find = array();
+
+		foreach ($tab_search as $x) {
+			$y = trim($x);
+			array_push($tab_find, $y);
+		}
+
+		$regex = new MongoDB\BSON\Regex ($search_id);
+		$result1 = $collection->find(['_id'=>$regex,$tab_find[0] => $tab_find[1]],['skip'=>$skip,'limit'=>$bypage])->toArray();
+
+		try{
+			$test = new MongoDB\BSON\ObjectId($search_id);
+			$result2 = $collection->find(['_id'=>$test,$tab_find[0] => $tab_find[1]],['skip'=>$skip,'limit'=>$bypage])->toArray();
+		}
+		catch(Exception $e){
+			$result2 = array();
+		}
+
+		$result = array_merge($result1,$result2);
+	}
+
+	return $result;
+
+}
+
+function getNbPages($result,$pages){
+	$nb = intval($result/$pages)+1;
+	return $nb;
+}
+
+function getNbPages_search($result,$pages){
+	$nb = intval(sizeof($result)/$pages)+1;
+	return $nb;
+}
+
+function countSearch_id($search){
+	require 'vendor/autoload.php';
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+	$db = strval($_SESSION['db']);
+	$collec = strval($_SESSION['collection']);
+	$collection = $client->$db->$collec;
+	
+	$regex = new MongoDB\BSON\Regex ($search);
+	$result1 = $collection->count(['_id'=>$regex]);
+
+	try{
+		$test = new MongoDB\BSON\ObjectId($search);
+		$result2 = $collection->count(['_id'=>$test]);
+	}
+	catch(Exception $e){
+		$result2 = 0;
+	}
+
+	$result = $result1+$result2;
+
+	return $result;
+}
+
+function countSearch_g($search)
+{
+	require 'vendor/autoload.php';
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+	$db = strval($_SESSION['db']);
+	$collec = strval($_SESSION['collection']);
+	$collection = $client->$db->$collec;
+
+	$tab_search = explode('=', $search);
+
+	$tab_find = array();
+
+	foreach ($tab_search as $x) {
+		$y = trim($x);
+		array_push($tab_find, $y);
+	}
+
+	$result = $collection->count([$tab_find[0] => $tab_find[1]]);
+
+
+	return $result;
+}
+
+
+function countSearch($search_id,$search_g)
+{
+	if($search_id == ''){
+
+		$result = countSearch_g($search_g);
+	}
+
+	elseif($search_g=='field = content[...]'){
+		$result = countSearch_id($search_id);
+	}
+
+	else{
+
+		require 'vendor/autoload.php';
+		$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+		$db = strval($_SESSION['db']);
+		$collec = strval($_SESSION['collection']);
+		$collection = $client->$db->$collec;
+
+		$tab_search = explode('=', $search_g);
+
+		$tab_find = array();
+
+		foreach ($tab_search as $x) {
+			$y = trim($x);
+			array_push($tab_find, $y);
+		}
+
+		$regex = new MongoDB\BSON\Regex ($search_id);
+		$result1 = $collection->count(['_id'=>$regex,$tab_find[0] => $tab_find[1]]);
+
+		try{
+			$test = new MongoDB\BSON\ObjectId($search_id);
+			$result2 = $collection->count(['_id'=>$test,$tab_find[0] => $tab_find[1]]);
+		}
+		catch(Exception $e){
+			$result2 = 0;
+		}
+
+		$result = $result1 + $result2;
+	}
+
+	return $result;
+}
+
+function getCollections()
+{
+	require 'vendor/autoload.php';
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+	$db = strval($_SESSION['db']);
+	$database = $client->$db;
+
+	$result = $database->listCollections();
+	
+	return $result;
+}
+
+function getDbs()
+{
+	require 'vendor/autoload.php';
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+
+	$result = $client->listDatabases();
+
+	return $result;
+}
+
+function renameCollec($newName)
+{
+	require 'vendor/autoload.php';
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+
+	$database = $client->admin;
+
+	$database->command(array('renameCollection'=>$_SESSION['db'].'.'.$_GET['coll_id'],'to'=>$_SESSION['db'].'.'.$newName));
+}
+
+function createCollec($name)
+{
+	require 'vendor/autoload.php';
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+
+	$db = strval($_SESSION['db']);
+
+	$database = $client->$db;
+
+	$database->createCollection($name);
+}
+
+function deleteColl()
+{
+	require 'vendor/autoload.php';
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+
+	$db = strval($_SESSION['db']);
+
+	$database = $client->$db;
+
+	$database->dropCollection($_GET['id']);
+}
+
+
+function moveCollec($db)
+{
+	require 'vendor/autoload.php';
+	$client = new MongoDB\Client('mongodb://'.$_SESSION['serve'].':27017');
+
+	$database = $client->admin;
+
+	$database->command(array('renameCollection'=>$_SESSION['db'].'.'.$_GET['coll_id'],'to'=>$db.'.'.$_GET['coll_id']));
 }
