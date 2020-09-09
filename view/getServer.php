79c115b73f5267aeffe23c45da7c763e2c73fa29
Sidi Sokhona <ssokhona@24eme.fr>
2020-09-09 17:19:07 +0200

    organisation de la page serveur

--- view/getServer.php
+++ view/getServer.php
@@ -3,23 +3,25 @@
 <head>
 	<?php echo "<title>".$serve."</title>"?>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
-<body>
+<body style="background-color:#A48340;">
 
 <?php
 
-echo "<h1 class='title'>".$serve."</h1>";
+echo "<h1 align='center' class='title'>IP: ".$serve."</h1>";
 
 ?>
 
-<div id="main">
+<div id="main" class="col-lg-6 offset-lg-3 mt-5 bg-light">
 	<br>
-	<table>
+	<table class="table">
 		<?php
 			foreach ($dbs as $db) {
 				echo '<tr>';
-				echo '<td><a href="index.php?action=getDb&serve='.$serve.'&db='.$db->getName().'">';
+				echo "<td><a class='text-dark' href='index.php?action=getDb&serve=".$serve."&db=".$db->getName()."'>";
+				// echo "<td><a  class='text-dark' href='index.php?action=getServer&serve=".$x."'>";
 				echo $db->getName();
 				echo '</a></td>';
 				echo '</tr>';
