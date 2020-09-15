53ac26c9e81effd1630206ec0f236b86986950ff
Rilerss <rilerss.ravendar@gmail.com>
2020-09-15 14:51:00 +0200

    ergo new doc mode

--- view/createDocument.php
+++ view/createDocument.php
@@ -3,25 +3,65 @@
 <head>
 	<title>Create Document</title>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link href="public/css/breadcrumb.css" rel="stylesheet" type="text/css">
+	<link href="public/css/createDocument.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 
 <?php
-echo '<span>';
-echo '<form method="post" action="index.php?action=thread">';
-echo '<input type="hidden" name="action_thread" value="'.$_GET['action'].'"></input>';
-if(isset($_GET['serve'])){echo '<label>Server: </label><input type="search" name="serve_thread" id="serve_thread" value="'.$_GET['serve'].'"/>';}
-else{echo '<label>Server: </label><input type="search" name="serve_thread" id="serve_thread"/>';}
-if(isset($_GET['db'])){echo '-><label>Database: </label><input type="search" name="db_thread" id="db_thread" value="'.$_GET['db'].'"/>';}
-else{echo '-><label>Database: </label><input type="search" name="db_thread" id="db_thread"/>';}
-if(isset($_GET['coll'])){echo '-><label>Collection: </label><input type="search" name="coll_thread" id="coll_thread" value="'.$_GET['coll'].'"/>';}
-else{echo '-><label>Collection: </label><input type="search" name="coll_thread" id="coll_thread"/>';}
-if(isset($_GET['doc'])){echo '-><label>Document: </label><input type="search" name="doc_thread" id="doc_thread" value="'.$_GET['doc'].'"/>';}
-else{echo '-><label>Document: </label><input type="search" name="doc_thread" id="doc_thread"/>';}
-echo '<input type="submit" name="go" id="go" value="Go"/>';
-echo '</form>';
-echo '</span>';
+
+
+	//Fil d'Ariane
+
+	echo '<nav>';
+		echo '<ol class="breadcrumb">';
+			echo '<li class="breadcrumb-item"><a href="index.php?">Home</a></li>';
+			if(isset($_GET['serve'])){
+				if($_GET['action']=='getServer'){
+					echo '<li class="breadcrumb-item active">'.$_GET['serve'].'</li>';
+				}
+				else{
+					echo '<li class="breadcrumb-item"><a href="index.php?action=getServer&serve='.$_GET['serve'].'">'.$_GET['serve'].'</a></li>';
+				}
+			}
+			if(isset($_GET['db'])){
+				if($_GET['action']=='getDb'){
+					echo '<li class="breadcrumb-item active">'.$_GET['db'].'</li>';
+				}
+				else{
+					echo '<li class="breadcrumb-item"><a href="index.php?action=getDb&serve='.$_GET['serve'].'&db='.$_GET['db'].'">'.$_GET['db'].'</a></li>';
+				}
+			}
+			if(isset($_GET['coll'])){
+				if($_GET['action']=='getCollection' or $_GET['action']=='getCollection_search'){
+					echo '<li class="breadcrumb-item active">'.$_GET['coll'].'</li>';
+				}
+				else{
+					echo '<li class="breadcrumb-item"><a href="index.php?action=getCollection&serve='.$_GET['serve'].'&db='.$_GET['db'].'&coll='.$_GET['coll'].'">'.$_GET['coll'].'</a></li>';
+				}
+			}
+			if(isset($_GET['doc'])){
+				echo '<li class="breadcrumb-item active">'.$_GET['doc'].'</li>';
+			}
+		echo '</ol>';
+	echo '</nav>';
+
+// echo '<span>';
+// echo '<form method="post" action="index.php?action=thread">';
+// echo '<input type="hidden" name="action_thread" value="'.$_GET['action'].'"></input>';
+// if(isset($_GET['serve'])){echo '<label>Server: </label><input type="search" name="serve_thread" id="serve_thread" value="'.$_GET['serve'].'"/>';}
+// else{echo '<label>Server: </label><input type="search" name="serve_thread" id="serve_thread"/>';}
+// if(isset($_GET['db'])){echo '-><label>Database: </label><input type="search" name="db_thread" id="db_thread" value="'.$_GET['db'].'"/>';}
+// else{echo '-><label>Database: </label><input type="search" name="db_thread" id="db_thread"/>';}
+// if(isset($_GET['coll'])){echo '-><label>Collection: </label><input type="search" name="coll_thread" id="coll_thread" value="'.$_GET['coll'].'"/>';}
+// else{echo '-><label>Collection: </label><input type="search" name="coll_thread" id="coll_thread"/>';}
+// if(isset($_GET['doc'])){echo '-><label>Document: </label><input type="search" name="doc_thread" id="doc_thread" value="'.$_GET['doc'].'"/>';}
+// else{echo '-><label>Document: </label><input type="search" name="doc_thread" id="doc_thread"/>';}
+// echo '<input type="submit" name="go" id="go" value="Go"/>';
+// echo '</form>';
+// echo '</span>';
 
 echo '<h1 class = "title">New Document</h1>';
 ?>
@@ -33,8 +73,8 @@ echo '<h1 class = "title">New Document</h1>';
 	 	$doc['example_field']='content[...]';
 	 	$docs = stripslashes(json_encode($doc,JSON_PRETTY_PRINT));
 	 	echo '<form method="post" action="index.php?action=traitement_nD&serve='.$_GET['serve'].'&db='.$_GET['db'].'&coll='.$_GET['coll'].'">';
-	 	echo '<textarea name="doc_text" id="doc_text" rows="20" cols="200" required>'.$docs.'</textarea>';
-	 	echo '<input type="submit" name="create" id="create" value="Create">';
+	 	echo '<div id="create_content"><input type="submit" class="btn btn-primary" name="create" id="create" value="Create"></div>';
+	 	echo '<div id="doc_content"><textarea name="doc_text" id="doc_text" rows="20" cols="200" required>'.$docs.'</textarea></div>';
 	 	echo '</form>';
 	 	if(isset($_GET['search'])){
 	 		echo '<a href="index.php?action=getCollection_search&serve='.$_GET['serve'].'&db='.$_GET['db'].'&coll='.$_GET['coll'].'&s_id='.$_GET['s_id'].'&s_g='.$_GET['s_g'].'&page='.$_GET['search'].'">< Collection</a>';
