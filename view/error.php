00d95e29cc5f7aa97e9f77e4de57c92303863557
Rilerss <rilerss.ravendar@gmail.com>
2020-09-24 17:18:28 +0200

    css error

--- view/error.php
+++ view/error.php
@@ -1,2 +1,23 @@
-<p>La page que vous avez demandé n'existe pas</p>
-<a href="index.php">Retour à l'accueil</a>
+<!doctype html>
+<html lang="fr">
+<head>
+	<title>Error</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link href="public/css/breadcrumb.css" rel="stylesheet" type="text/css">
+	<link href="public/css/titre.css" rel="stylesheet" type="text/css">
+	<link href="public/css/btn_return.css" rel="stylesheet" type="text/css">
+	<link href="public/css/error.css" rel="stylesheet" type="text/css">
+	<script src="public/js/db.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<br><br>
+	<h1 align='center' class='title'><i class="fa fa-fw fa-exclamation-circle"></i>Error</h1>
+	<div class="error_content">
+		<p>The page you requested does not exist.</p>
+		<a href="index.php"><button class="return btn btn-primary">< Home</button></a>
+	</div>
+</body>
+</html>
