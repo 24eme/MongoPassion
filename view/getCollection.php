5d8dd6ae2a8502b7427abfc4ff397cddf14e9631
Sidi Sokhona <ssokhona@24eme.fr>
2020-09-30 18:17:42 +0200

    suppression des tous les modals utilisation de button show

--- view/getCollection.php
+++ view/getCollection.php
@@ -14,10 +14,7 @@
 	<link href="public/css/pagination.css" rel="stylesheet" type="text/css">
 
  	<script src="public/js/db.js"></script> 
-	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+	
 
 
 </head>
@@ -105,23 +102,25 @@ echo ' of '.$nbDocs.'</h2>';
   <i class="fa fa-fw fa-search"></i>Search by command
 </button>
 </div>
-<!-- Modal Start -->
-<div class="modal" id="myModal">
-  <div class="modal-dialog">
-    <div class="modal-content">
 
-      <!-- Modal Header -->
-      <div class="modal-header">
-        <h4 class="modal-title">Search</h4>
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-      </div>
 
-      <!-- Modal body -->
-      <div class="modal-body">
-      		<!-- <div  class="m-auto border border-success col-lg-6 offset-lg-3 bg-light mt-1"> -->
-		<!-- <h3 class="text-center bg-success text-light"><span><strong>ESPACE OF SEARCH</strong></span></h3> -->
-		<!-- <hr> -->
-	<div >
+
+
+</nav>
+
+
+	   <div id="command" class="border col-lg-6 offset-lg-3 bg-light m-auto mb-">
+		  	<label>Search by command: </label>
+		<?php echo '<form method="post" action="index.php?action=getCollection_search&serve='.$_GET['serve'].'&db='.$_GET['db'].'&coll='.$_GET['coll'].'">'; ?>
+	<div class="input-group mb-3">
+		<input type="search" class="form-control" name="special_search" id="special_search" size=100 value="find( ['_id'=>'CONTRAT-000013-20130812-0001'])"/>
+		<input type="submit" class="btn  bg-success text-light " name="search" id="search" value="Search"/>
+	</div>
+	</form>
+</div>
+
+<div id="searchId" class="border col-lg-6 offset-lg-3 bg-light m-auto mb-2">
+
 	<label for="pet-select">Search:</label>
 		
 
@@ -154,37 +153,9 @@ echo ' of '.$nbDocs.'</h2>';
 
 
 	</form>
+
 </div>
-     
-    </div>
-  </div>
-</div>
-</div>
- <!-- modalEnd -->
 
-
-
-</nav>
-
-<!-- <hr> -->
-
-
-<!-- </div> -->
-<!-- </div> -->
-
-
-
-
-<!-- <div id="main" class="border col-lg-5 bg-light mt-1"> -->
-	   <div id="command" class="border col-lg-6 offset-lg-3 bg-light m-auto mb-2">
-		  	<label>Search by command: </label>
-		<?php echo '<form method="post" action="index.php?action=getCollection_search&serve='.$_GET['serve'].'&db='.$_GET['db'].'&coll='.$_GET['coll'].'">'; ?>
-	<div class="input-group mb-3">
-		<input type="search" class="form-control" name="special_search" id="special_search" size=100 value="find( ['_id'=>'CONTRAT-000013-20130812-0001'])"/>
-		<input type="submit" class="btn  bg-success text-light " name="search" id="search" value="Search"/>
-	</div>
-	</form>
-</div>
 
 	
 	<div id="main" class="border col-lg-6 offset-lg-3 bg-light m-auto mt-1">
