4a7b4592196c56a5db60108d26a17757f4cd0949
Rilerss <rilerss.ravendar@gmail.com>
2020-10-12 16:48:35 +0200

    home

--- view/home.php
+++ view/home.php
@@ -9,25 +9,27 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link href="public/css/titre.css" rel="stylesheet" type="text/css">
 	<link href="public/css/btn_return.css" rel="stylesheet" type="text/css">
+	<link href="public/css/home.css" rel="stylesheet" type="text/css">
 </head>
 
 <div id="main" class=" text-center ">
-	<br><br>
 
 	<!-- Titre de la page -->
 
-	<h1 class='title font-weight-bold'align="center">MongoCRUD</h1>
+	<div class ='title' align="center">
+		<img align="center" src="public/images/mongo.png" alt="24eme">
+		<h1 class='title font-weight-bold'align="center">mongoCRUD</h1>
+	</div>
 
 	<!-- Fin du titre de la page -->
 
-	<img align="center"  class="rounded-circle center  w-25 m-auto" src="public/images/mongodb.jpg" alt="24eme">
-
-
 	<!-- Formulaire serveurs -->
 
-	<form align="center" method="post" class="col-lg-8 offset-lg-2 " action="index.php?action=getServer">
-		<input type="text" class="form-control border border-success" name="serve" id="serve" placeholder="Add your IP address" maxLength = 20 required /> 
-		<input type="submit" class="btn btn-success btn-sm mt-1 font-weight-bold" name="add" id="add" value="ADD">
+	<form method="post" class="col-lg-8 offset-lg-2 " action="index.php?action=getServer">
+		<div>
+			<input type="text" class="form-control border border-success" name="serve" id="serve" placeholder="Add your IP address" maxLength = 20 required /> 
+			<input type="submit" class="btn btn-success font-weight-bold" name="add" id="add" value="ADD">
+		</div>
 	</form>
 
 	<!-- Fin du formulaire serveurs -->
@@ -41,12 +43,13 @@
 		$serve_list=json_decode($_COOKIE['serve_list']);
 		if(sizeof($serve_list)>0){
 			echo '<table class="table table-sm table-striped ">';
-			echo  	"<h3 class=\"text-center bg-success text-light\"><span><strong>Server list <i class=\"fa fa-fw fa-desktop\"></i></strong></span></h3>" ;
+			echo  	"<h3 class=\"text-center bg-success text-light\"><span><strong>Server list </strong></span></h3>" ;
 			foreach ($serve_list as $x) {
 				echo '<tr>';
 				echo "<td><a  class='text-success' href='index.php?action=getServer&serve=".$x."'><i class=' text-dark mr-2 fa fa-fw fa-desktop'></i>";
 				echo $x;
 				echo '</a></td>';
+				echo '<td><i class="fa fa-fw fa-remove"></i></td>';
 				echo '</tr>';
 			}
 			echo '</table>';
