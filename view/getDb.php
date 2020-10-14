d453811ed7073bdcd9356e61063aa01c41e66a53
Sidi Sokhona <ssokhona@24eme.fr>
2020-10-14 11:27:27 +0200

    ajout des bouton close dans des modals

--- view/getDb.php
+++ view/getDb.php
@@ -88,6 +88,9 @@ echo '</div>';
 							</form>
 						</div>
 				 </div>
+				<div class="modal-footer">
+        		    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
 			</div>
 		</div>
 	</div>
--- view/getDb_search.php
+++ view/getDb_search.php
@@ -97,6 +97,9 @@ echo '</div>';
 							</form>
 						</div>
 				 </div>
+				<div class="modal-footer">
+        		    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
 			
 
 
