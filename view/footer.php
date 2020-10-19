69057bbe542491016c1016237bc4696729d661d6
Sidi Sokhona <ssokhona@24eme.fr>
2020-10-19 13:22:45 +0200

    footer.php qui contient le footer

--- view/footer.php
+++ view/footer.php
@@ -1,7 +1,7 @@
 <?php
 
 echo
-"<footer  style=\"background-color:#ececec;\" class=\"container border-x  border-success footer fixed-bottom font-weight-bold col-lg-8 offset-lg-2  text-success text-center py-3\">© 2020 Copyright:
+"<footer  style=\"background-color:#ececec;\" class=\"container footer fixed-bottom font-weight-bold col-lg-8 offset-lg-2  text-success text-center py-3\">© 2020 Copyright:
     <a class=\"text-success\" href=\"https://github.com/24eme/Interface-MongoDB\">MongoDoAllCRUD<i class=\"text-success fa fa-fw fa-github\"></i></a>
   </footer>";
 
