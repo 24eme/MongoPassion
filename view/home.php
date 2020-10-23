5642b6560a6cf1340b195789654ad2ef59259ed9
Tangui Morlier <tangui@tangui.eu.org>
2020-10-23 16:48:33 +0200

    Modal working

--- view/home.php
+++ view/home.php
@@ -117,7 +117,7 @@
 	      </div>
 	      <div class="modal-footer">
 	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-	        <button type="button" class="btn btn-success" type="submit">Connection</button>
+	        <button type="submit" class="btn btn-success">Connect</button>
 	      </div>
 	    </div>
 	  </div>
