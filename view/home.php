15ac5fd64e6c34a4816fad52d23012d483bd440c
Sidi Sokhona <ssokhona@24eme.fr>
2020-10-23 13:01:47 +0200

    Titre sur les bouton à picto dans la page home

--- view/home.php
+++ view/home.php
@@ -77,10 +77,10 @@
 			echo  	"<h3 class=\"text-center bg-success text-light\"><span><strong>Server list </strong></span></h3>" ;
 			foreach ($serve_list as $x) {
 				echo '<tr>';
-				echo "<td><a  class='text-success' href='index.php?action=getServer&serve=".$x."'><i class=' text-dark mr-2 fa fa-fw fa-desktop'></i>";
+				echo "<td><a  class='text-success'  href='index.php?action=getServer&serve=".$x."'><i title='address IP of server' class='text-dark mr-2 fa fa-fw fa-desktop'></i>";
 				echo $x;
 				echo '</a></td>';
-				echo '<td><a href="?action=removeServer&serve='.$x.'"><i class="fa fa-fw fa-remove"></i></a></td>';
+				echo '<td><a href="?action=removeServer&serve='.$x.'"><i title="Delete server" class="fa fa-fw fa-remove"></i></a></td>';
 				echo '</tr>';
 			}
 			echo '</table>';
