afc44b0d1646346f18f0c41e801069bf11e4d685
Sidi Sokhona <ssokhona@24eme.fr>
2020-10-26 11:00:44 +0100

    fleche pagination à gauche dans getCollection & getCollection_search

--- view/getCollection.php
+++ view/getCollection.php
@@ -66,10 +66,10 @@ echo "<h1 class='title text-center font-weight-bold'><i title='Name of collectio
 <!-- Tableau des documents de la collection -->
 
 <div id="DivContentTable">
-	<div id="result" class="border bg-light m-auto getCollDiv">
+	<div id="result" class="border bg-light m-auto ">
 		<?php include('tableauDocuments.php'); ?>
 	    <hr>
-		<div class="row  justify-content-between">
+		<div class="row  justify-content-between  mt-3 mx-1">
 
 				<!-- Bouton de retour -->
 
@@ -83,33 +83,50 @@ echo "<h1 class='title text-center font-weight-bold'><i title='Name of collectio
 
 
 				<!-- Pagination -->
-				<div class="row mr-2">
-					<div >
-					<?php if($page!=1): ?>
+				<div class="row">
+					<!-- <div > -->
+
+				<!-- 	<?php if($page!=1): ?>
 					<a href="index.php?action=getCollection&serve=".<?= $serve ?>."&db=".<?= $db ?>."&coll=".<?= $coll ?>."&page=".<?= ($page-1) ?>."&bypage=".<?= $bypage ?>."\" id="prev" aria-current="page"><span aria-hidden="true">&laquo;</span></a>
 				         <?php else : ?>
 				            <span id="prev"><span aria-hidden="true">&laquo;</span></span>
-				         <?php endif ?>
+				         <?php endif ?> -->
 
-					 <h6 class="mr-2 pt-2">Documents <?= (1+(($page-1)*$bypage)) ?> -
-						<?php if(($page*$bypage)<$nbDocs): ?>
-							<?= $page*$bypage; ?>
-						<?php else: ?>
-							<?= $nbDocs . ' of '.$nbDocs ?>
-						<?php endif; ?>
-					</h6>
-					</div>
-					<div class="text-center" aria-label="pagination" >
+                    <div aria-label="pagination " >
 				        <ul class="pagination">
 
+						      <?php
+					            if($page!=1){
+					            	echo '<a href="index.php?action=getCollection&serve='.$serve.'&db='.$db.'&coll='.$coll.'&page='.($page-1).'&bypage='.$bypage.'&s_g='.urlencode($recherche_g).'" id="prev" aria-current="page"><span aria-hidden="true">&laquo;</span></a>';
+					            }
+					            else{
+					            	echo '<span id="prev"><span aria-hidden="true">&laquo;</span></span>';
+					           } ?>
 
-				            <span  class="text-center bg-light font-weight-bold mr-1">
-								<select id="select_pagination" name="bypage" onchange="bypage(this)">
-                    <?php foreach([10, 20, 30, 50] as $nb) : ?>
-                      <option value="<?= $nb ?>" <?= ($bypage == $nb) ? 'selected="selected"': '' ?>><?= $nb ?></option>
-                    <?php endforeach ?>
+							 
+						</ul>
+					 </div>
+					 <div class="mx-1">
+						 <h6 class=" pt-2">Documents <?= (1+(($page-1)*$bypage)) ?> -
+									<?php if(($page*$bypage)<$nbDocs): ?>
+										<?= $page*$bypage; ?>
+									<?php else: ?>
+										<?= $nbDocs . ' of '.$nbDocs ?>
+									<?php endif; ?>
+						 </h6>
+					</div>
+					
+                    <div>
+				            <span  class="text-center bg-light p-0 font-weight-bold mr-1">
+								<select id="select_pagination" class="py-1" name="bypage" onchange="bypage(this)">
+                    		<?php foreach([10, 20, 30, 50] as $nb) : ?>
+                     			 <option value="<?= $nb ?>" <?= ($bypage == $nb) ? 'selected="selected"': '' ?>><?= $nb ?></option>
+                   			 <?php endforeach ?>
 								</select>
 							</span>
+                    </div>
+                   <div aria-label="pagination" class="ml-2 " >
+				        <ul class="pagination pb-3">
 
 				            <?php if($page!=$nbPages){
 				            	echo '<a href="index.php?action=getCollection&serve='.$serve.'&db='.$db.'&coll='.$coll.'&page='.($page+1).'&bypage='.$bypage.'" id="next" aria-current="page"><span aria-hidden="true">&raquo;</span></a>';
@@ -126,7 +143,7 @@ echo "<h1 class='title text-center font-weight-bold'><i title='Name of collectio
 				</div>
 			   		<!-- Bouton nouveau document -->
 				<div class="ml-2">
-					    <?php echo '<button class="btn btn-dark py-1 font-weight-bold"><a class="text-light" href="index.php?action=createDocument&serve='.$serve.'&db='.$db.'&coll='.$coll.'"><i title="Create new doc"class="fa fa-fw fa-plus"></i><i title="Create new doc" class="fa fa-fw fa-file"></i></a></button>'; ?>
+					    <?php echo '<button class="btn btn-dark py-1 font-weight-bold"><a class="text-light" href="index.php?action=createDocument&serve='.$serve.'&db='.$db.'&coll='.$coll.'"><i title="Create new doc"class="fa fa-fw fa-plus"></i><i title="Create new doc" class="fa fa-fw fa-book"></i></a></button>'; ?>
 				</div>
 			  <!-- Fin du bouton nouveau document -->
 
