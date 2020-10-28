2a6e0c09e6c35e1fea594873b66a2bee5a928df4
Sidi Sokhona <ssokhona@24eme.fr>
2020-10-28 17:35:30 +0100

    layout (in progress)

--- view/editDocument.php
+++ view/editDocument.php
@@ -19,7 +19,7 @@ $link_d = 'index.php?action=deleteDocument&serve='.$serve.'&db='.$db.'&coll='.$c
 
 //Titre de la page
 
-echo "<h2 class='title text-center'>Edit <i title='id of document' class='fa fa-fw fa-file'></i>".$doc."<button  class=\"btn \"><a class=\"text-danger font-weight-bold\" href=".$link_d." onclick=\"return confirmDelete()\"><i title='Delete this document'class='fa fa-2x fa-trash'></i></a></button></h2>";
+echo "<h2 class='title text-center'>Edit <i title='id of document' class='fa fa-fw fa-book'></i>".$doc."<button  class=\"btn \"><a class=\"text-danger font-weight-bold\" href=".$link_d." onclick=\"return confirmDelete()\"><i title='Delete this document'class='fa fa-2x fa-trash'></i></a></button></h2>";
 
 //Fin du titre de la page
 
@@ -97,40 +97,12 @@ if(isset($search_db)){
 		}
 	}
 	?>
-	<?php
-		//Formatage des données du document en JSON
+    <!-- Formatage des données du document en JSON -->
+	<?php include('layouts/formatJsonInEditDocument.php'); ?>
+     <!--Fin formatage des données du document en JSON -->
 
-		foreach ($result as $entry) {
-			$doc=array();
-			$date_array=array();
-			$up_date_array=array();
-		    foreach($entry as $x => $x_value) {
-		 		if(gettype($x_value)=='object' and get_class($x_value)=='MongoDB\BSON\ObjectId'){
-		 			$value = $x_value;
-		 		}
-		 		elseif(gettype($x_value)=='object' and get_class($x_value)=='MongoDB\BSON\UTCDateTime'){
-		 			$value = $x_value->toDateTime();
-		 			$date_array[$x]=intval((string)$x_value);
-		 			$temp=strtotime((improved_var_export(printable($value))['date']))*1000;
-		 			$up_date_array[$x]=$temp;
-		 		}
-		 		else{
-		 	  		$value = printable($x_value);
-		 		}
-		 		$doc[$x] = improved_var_export($value);
-		 	}
-		 	$doc = init_json($doc);
-		 	//contenu de l'erreur
-		 	 if(isset($_GET['doc_text'])){
-		 		$docs= $_GET['doc_text'];
-		 		$docs = json_decode($docs);
-		 		$docs = $docs->data;
-		 	 }else{
-		 	 	$docs = stripslashes(json_encode($doc,JSON_PRETTY_PRINT));
-		 	 }
-
-	 	}
-
+	
+    <?php
 	 	//Affichage du formulaire
 
 	 	echo '<form method="post" action="'.$link_doc.'">';
@@ -173,54 +145,8 @@ if(isset($search_db)){
 <?php }?>
 
 <!-- Script de création et d'envoi du formulaire -->
-
-<script type="text/javascript">
-    const container = document.getElementById("jsoneditor")
-    const options = {}
-    const editor = new JSONEditor(container, options)
-    var variableRecuperee = <?php echo stripslashes(json_encode($doc)); ?>;
-    var link_doc = <?php echo (json_encode($link_doc)); ?>;
-
-    /* Création du formulaire */
-
-    const initialJson = variableRecuperee;
-    editor.set(initialJson);
-    editor.expandAll();
-
-    /* Fonction d'envoi du formulaire */
-
-    document.getElementById('getJSON').onclick = function () {
-        getJson();
-
-    }
-    document.getElementById('getJSON2').onclick = function () {
-        getJson();
-
-    }
-
-    function getJson(){
-    	const json = editor.get()
-        var updatedJson = JSON.stringify(json, null, 2)
-
-        var f = document.createElement("form");
-        f.setAttribute('method',"post");
-        f.setAttribute('action',link_doc);
-        f.setAttribute('id',"idFormulaire");
-
-        var i = document.createElement("input"); //input element, text
-        i.setAttribute('type',"hidden");
-        i.setAttribute('name',"doc_text");
-        i.setAttribute('value',updatedJson);
-
-        f.appendChild(i);
-
-        var span = document.getElementById("nC");
-
-        span.appendChild(f);
-
-        document.getElementById("idFormulaire").submit();
-    }
-</script>
+<?php include('layouts/scriptOfCreateFormInCreateDocument.php'); ?>
+<!-- Fin Script de création et d'envoi du formulaire -->
 
 <!-- Fin du formulaire mode édition JsonEditor -->
 
@@ -241,13 +167,15 @@ echo '<div id="nav_view float-left">';
 		echo '<a href="index.php?action=getCollection&serve='.$serve.'&db='.$db.'&coll='.$coll.'&page='.$page.'"><button class="return btn btn-primary">< list of docs</button></a>';
 	}
 echo '</div>';
+
+//Fin bouton de retour
 ?>
 
 
-</div>
+
 
 <!-- footer -->
-<br><br>
+
 <?php
 	require_once('layouts/footer.php')
 ?>
