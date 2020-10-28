ca160e065c404f1fee86485cd02b9a8add00b3e1
Sidi Sokhona <ssokhona@24eme.fr>
2020-10-28 15:18:08 +0100

    layout (in progress)

--- view/createDocument.php
+++ view/createDocument.php
@@ -13,7 +13,7 @@
 
 //Titre de la page
 
-echo '<h1 class = "title text-center font-weight-bold"><i title="Create new document" class="fa fa-fw fa-file"></i> New Document</h1>';
+echo '<h1 class = "title text-center font-weight-bold"><i title="Create new document" class="fa fa-fw fa-book"></i> New Document</h1>';
 
 //Fin titre de la page
 
@@ -30,20 +30,8 @@ if(isset($_GET['msg'])){
 
 }
 
-//Bouton de retour
 
-echo '<div id="nav_view">';
-	if(isset($s_g)){
-	 	echo '<a href="index.php?action=getCollection_search&serve='.$serve.'&db='.$db.'&coll='.$coll.'&s_g='.$s_g.'"><button class="return btn btn-primary">< Collection</button></a>';
-	}
-	else{
-	 	echo '<a href="index.php?action=getCollection&serve='.$serve.'&db='.$db.'&coll='.$coll.'"><button class="return btn btn-primary">< Collection</button></a>';
-	}
-echo '</div>';
 ?>
-
-<!-- Fin du bouton de retour -->
-
 
 <!-- Bouton switch JsonEditor -->
 
@@ -68,37 +56,9 @@ echo '</div>';
 
 <!-- Zone de texte -->
 
-<div id="DivContentTable">
-	<?php
-	if (isset($_GET['input']) && ($_GET['input'] === 'true')) {
-		echo '<div id="main" class="creatDocDiv" style="display: block">';
-	} else {
-		if($jsoneditor){
-			echo '<div id="main" class="creatDocDiv" style="display: none">';
-		}
-		else{
-			echo '<div id="main" class="creatDocDiv">';
-		}
-	}
-	?>
-		<?php
-			$link_doc = 'index.php?action=traitement_nD&serve='.$serve.'&db='.$db.'&coll='.$coll.'';
-		 	$doc = array();
-		 	$doc['example_field']='content[...]';
-		    if(isset($_GET['doc_text'])){
-		 		$docs= $_GET['doc_text'];
-		 		$docs = json_decode($docs);
-		 		$docs = $docs->data;
-			} else {
-		 		$docs = stripslashes(json_encode($doc,JSON_PRETTY_PRINT));
-		    }
-		 	echo '<form method="post" action="index.php?action=traitement_nD&serve='.$serve.'&db='.$db.'&coll='.$coll.'">';
-		 	echo '<div id="create_content"><input type="submit" class="btn btn-primary" name="create" id="create" value="Create"></div>';
-		 	echo '<div id="doc_content"><textarea class="col-lg-8 offset-lg-2" name="doc_text" id="doc_text" rows="20" cols="200" style="height: 750px;" required>'.$docs.'</textarea></div>';
-		 	echo '</form>';
-		?>
-	</div>
-</div>
+
+<?php include('layouts/zoneTextareaInCreateDocument.php'); ?>
+
 
 <!-- Fin de la zone de texte -->
 
@@ -125,51 +85,31 @@ echo '</div>';
 	 </div>
 
 	</div>
+	<?php
+// fiin Affichage du formulaire 
+
+
+//Bouton de retour
+
+echo '<div id="nav_view float-left">';
+	if(isset($s_g)){
+	 	echo '<a href="index.php?action=getCollection_search&serve='.$serve.'&db='.$db.'&coll='.$coll.'&s_g='.$s_g.'"><button class="return btn btn-primary">< Collection</button></a>';
+	}
+	else{
+	 	echo '<a href="index.php?action=getCollection&serve='.$serve.'&db='.$db.'&coll='.$coll.'"><button class="return btn btn-primary ml-5">< Collection</button></a>';
+	}
+echo '</div>';
+?>
+
+<!-- Fin du bouton de retour -->
+
 </div>
 <?php }?>
 
+
+
 <!-- Script de création et d'envoi du formulaire -->
-
-<script type="text/javascript">
-    const container = document.getElementById("jsoneditor")
-    const options = {}
-    const editor = new JSONEditor(container, options)
-    var variableRecuperee = <?php echo stripslashes(json_encode($doc)); ?>;
-    var link_doc = <?php echo (json_encode($link_doc)); ?>;
-
-    /* Création du formulaire */
-
-    const initialJson = variableRecuperee;
-    editor.set(initialJson);
-    editor.expandAll();
-
-    /* Fonction d'envoi du formulaire */
-
-    document.getElementById('getJSON').onclick = function () {
-        const json = editor.get()
-        var updatedJson = JSON.stringify(json, null, 2)
-
-        var f = document.createElement("form");
-        f.setAttribute('method',"post");
-        f.setAttribute('action',link_doc);
-        f.setAttribute('id',"idFormulaire");
-
-        var i = document.createElement("input"); //input element, text
-        i.setAttribute('type',"hidden");
-        i.setAttribute('name',"doc_text");
-        i.setAttribute('value',updatedJson);
-
-        f.appendChild(i);
-
-        var span = document.getElementById("nC");
-
-        span.appendChild(f);
-
-        document.getElementById("idFormulaire").submit();
-  
-
-    }
-</script>
+<?php include('layouts/scriptOfCreateFormInCreateDocument.php'); ?>
 
 <!-- Fin du formulaire mode édition JsonEditor -->
 
---
+++ view/layouts/scriptOfCreateFormInCreateDocument.php
@@ -0,0 +1,41 @@
+
+<script type="text/javascript">
+    const container = document.getElementById("jsoneditor")
+    const options = {}
+    const editor = new JSONEditor(container, options)
+    var variableRecuperee = <?php echo stripslashes(json_encode($doc)); ?>;
+    var link_doc = <?php echo (json_encode($link_doc)); ?>;
+
+    /* Création du formulaire */
+
+    const initialJson = variableRecuperee;
+    editor.set(initialJson);
+    editor.expandAll();
+
+    /* Fonction d'envoi du formulaire */
+
+    document.getElementById('getJSON').onclick = function () {
+        const json = editor.get()
+        var updatedJson = JSON.stringify(json, null, 2)
+
+        var f = document.createElement("form");
+        f.setAttribute('method',"post");
+        f.setAttribute('action',link_doc);
+        f.setAttribute('id',"idFormulaire");
+
+        var i = document.createElement("input"); //input element, text
+        i.setAttribute('type',"hidden");
+        i.setAttribute('name',"doc_text");
+        i.setAttribute('value',updatedJson);
+
+        f.appendChild(i);
+
+        var span = document.getElementById("nC");
+
+        span.appendChild(f);
+
+        document.getElementById("idFormulaire").submit();
+  
+
+    }
+</script>
